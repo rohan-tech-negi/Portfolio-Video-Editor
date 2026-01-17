0e72e61570f4523fb7cd8de875ca99375adcc9a8
rohan-tech-negi <rohanwork953@gmail.com>
2026-01-18 03:34:27 +0530

    started the woork

---
+++ app/components/Navbar.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import LeafLogo from '../items/Logo';
+import Logo from '../items/Logo';
+
+const Navbar = () => {
+  return (
+    <nav className="flex items-center justify-between p-10 bg-white border-b">
+      {/* Left side (empty or add logo later) */}
+      <div></div>
+
+      {/* Center: Main navigation */}
+      <ul className="flex space-x-8 text-xl">
+        <li className="cursor-pointer hover:text-gray-700">Projects</li>
+        <li className="cursor-pointer hover:text-gray-700">About & Contact</li>
+      </ul>
+
+      {/* Right side: Email + Contact */}
+      <div className="flex items-center space-x-4">
+        <span className="text-sm text-gray-500">Email: Lorem ipsum dolor sit amet.</span>
+        <button className="px-4 py-2 bg-gray-800 text-white rounded-full text-sm font-medium hover:bg-gray-900">
+          Contact Me
+        </button>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ app/pages/Home.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LandingSection from '../sections/LandingSection'
+
+const Home = () => {
+  return (
+    <>
+        <LandingSection></LandingSection>
+    </>
+  )
+}
+
+export default Home
