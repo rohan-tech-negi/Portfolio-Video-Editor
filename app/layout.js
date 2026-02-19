6eb36b0de08795f49b45c14c55f728949e8cb0df
rohan-tech-negi <rohanwork953@gmail.com>
2026-02-19 10:47:10 +0530

    now continueing with the pagetransition

--- app/layout.js
+++ app/layout.js
@@ -24,13 +24,15 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
-  <Navbar />
+ 
 
   <PageTransition>
+ <Navbar />
     {children}
+ <Footer />
   </PageTransition>
 
-  <Footer />
+ 
 </body>
 
     </html>
