c532a274111d9466f137e46b301f16617bbc2e79
rohan-tech-negi <rohanwork953@gmail.com>
2026-03-17 15:13:43 +0530

    added a new portfolio pdf in the code

--- app/components/sections/Hero2.jsx
+++ app/components/sections/Hero2.jsx
@@ -150,7 +150,7 @@ export default function Home() {
               className="mt-10 origin-left"
             >
               <a
-                href="/resume.pdf"
+                href="/RohanResume.pdf"
                 download="Rohan_Singh_Negi_Resume.pdf"
                 onMouseEnter={() => setCursorText('DOWNLOAD')}
                 onMouseLeave={() => setCursorText(null)}
